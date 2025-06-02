b2e8779e46bfbeb456dbd807854401384171b469
starlkhorm <degeneralola@gmail.com>
2025-06-02 17:06:39 +0100

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("first task");
+console.log("second task");
+console.log("third task");
+console.log("fourth task");
+// for(let i=0;i<100000; i++ ){
+//     console.log("deleyyy")
+// }
+
+setTimeout
+
