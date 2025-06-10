9c2142f466639edbbe668f0a4120467575417950
starlkhorm <degeneralola@gmail.com>
2025-06-10 16:42:32 +0100

    commit

---
+++ tsclass.ts
@@ -0,0 +1,33 @@
+let myname: string ='ife';
+let score: number =70;
+
+myname =65;
+
+let secname ='jackson';
+
+secname =50;
+
+// function calculateIntresstRate(principal:number,rate:number,time:number) {
+//     return((principal * rate*time)/100)
+// }
+// calculateIntresstRate(5,5,5)
+
+// function getMyName( ) {
+//     let myname ='ife'
+//     return myname
+//     console.log('ffffff')
+// // }
+
+// function subtractTwoNumbers(): string {
+//     return ''
+// }
+// //return boolean
+//  function bully(): boolean {
+//     return true
+// }
+
+// function getHighestNumber(a:number,b :number,c:number,number):number {
+//   //return Math.max(a,b,c,d)
+//   return 'ffff'
+// }
+
